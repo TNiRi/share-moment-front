73615cd12a11fd28a98ebd4bf74081e1d489e925
TNiRi <goodluckgame@mail.ru>
2026-06-14 17:59:15 +0300

    Изменение конфига api_url

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
 const local_api_url = 'http://127.0.0.1:5000'
-const deploy_api_url = 'http://api:5000'
+const deploy_api_url = 'http://share-moment.ru:5000'
 
 export const api_url = process.env.VUE_APP_MODE === "production" ? deploy_api_url : local_api_url;
